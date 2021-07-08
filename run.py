615b848d49ac361f9be2f66212db9846028ff22a
Ben <fullerb@onemata.com>
2021-07-07 23:55:11 -0600

    working

--- run.py
+++ run.py
@@ -12,16 +12,18 @@ pd.read_csv(s3_client.get_object(
 )['Body'])
 
 # Read CSV using the Lambda Access Point
-pd.read_csv(s3_client.get_object(
+df=pd.read_csv(s3_client.get_object(
     Bucket=os.environ['LAP_ARN'],
     Key='weather.csv'
 )['Body'])
+#print(len(df.index))
+#print(df.head)
+#print(df.describe())
 
 # Read *Parquet* using the Lambda Access Point - dynamically generated!
 df=pd.read_parquet(BytesIO(s3_client.get_object(
     Bucket=os.environ['LAP_ARN'],
     Key='weather.parquet'
 )['Body'].read()))
-
-print('here')
-df.head()
+#
+print(df.head())
