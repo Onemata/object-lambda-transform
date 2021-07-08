2a00df2f3ebc628189726bcc2527b940b9b5c0bf
Ben <fullerb@onemata.com>
2021-07-07 23:45:16 -0600

    local updates

---
+++ run.py
@@ -0,0 +1,27 @@
+from io import BytesIO
+import pyarrow.parquet as pq
+import boto3
+import pandas as pd
+import os
+
+s3_client = boto3.client('s3')
+# Read CSV directly using the bucket
+pd.read_csv(s3_client.get_object(
+    Bucket=os.environ['BUCKET_NAME'],
+    Key='weather.csv'
+)['Body'])
+
+# Read CSV using the Lambda Access Point
+pd.read_csv(s3_client.get_object(
+    Bucket=os.environ['LAP_ARN'],
+    Key='weather.csv'
+)['Body'])
+
+# Read *Parquet* using the Lambda Access Point - dynamically generated!
+df=pd.read_parquet(BytesIO(s3_client.get_object(
+    Bucket=os.environ['LAP_ARN'],
+    Key='weather.parquet'
+)['Body'].read()))
+
+print('here')
+df.head()
